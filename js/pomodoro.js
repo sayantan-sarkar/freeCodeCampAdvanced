f64d8aac6a832e93fd4b5c6b5d0691b30dfcb14a
sayantan-sarkar <sayantansarkar24@gmail.com>
2017-10-25 03:54:31 +0530

    Static structure complete

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
+	var symbol,value;
+	$('.stopSym').css('cursor','pointer');
+	$('.workSym').css('cursor','pointer');
+
+	$('.stopSym').click(function(){
+		symbol = $(this).text();
+		value = parseInt($('.stopVal').text());
+		value = arith(value,symbol);
+		$('.stopVal').text(value);
+  	});
+
+	$('.workSym').click(function(){
+		symbol = $(this).text();
+		value = parseInt($('.workVal').text());
+		value = arith(value,symbol);
+		$('.workVal').text(value);
+  	});
 
 
 });
+
+
+function arith(value, symbol){
+	switch(symbol){
+			case '+': value = value+1;
+				break;
+			case '-': value = value-1;
+	}
+	if(value<1){
+		value = 1;
+	}
+	return(value);
+
+}
